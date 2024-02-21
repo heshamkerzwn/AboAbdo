8470ed5d6533005cfb9b2398959dd22e43aeeb6d
heshamkerzwn <heshamkarzwn@gmail.com>
2024-02-21 21:53:52 +0300

    edited the JS file

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const landing = document.querySelector(".landing");
 megaMenu.style.transition = "top 0.5s ease-in-out, opacity 0.5s ease-in-out;";
 megaMenu.style.opacity = 0;
 megaMenu.style.top = "calc(100% + 50px)"; // اضبط حسب الحاجة
+megaMenu.style.visibility = "hidden";
 
 function toggleMegaMenu() {
   test.classList.toggle("block");
@@ -14,17 +15,22 @@ function toggleMegaMenu() {
     megaMenu.style.opacity = 1;
     megaMenu.style.top = "calc(100% + 1px)";
     megaMenu.style.zIndex = "100";
+    megaMenu.style.visibility = "visible";
   } else {
     megaMenu.style.opacity = 0;
     megaMenu.style.top = "calc(100% + 50px)";
-    megaMenu.style.zIndex = "-99";
+    megaMenu.style.zIndex = "-1";
+    megaMenu.style.visibility = "hidden";
+    test.classList.remove("block");
   }
 }
 // قم بتوصيل مستمع الحدث
 landing.addEventListener("click", () => {
   megaMenu.style.opacity = 0;
   megaMenu.style.top = "calc(100% + 50px)";
-  megaMenu.style.zIndex = "-99";
+  megaMenu.style.zIndex = "-1";
+  megaMenu.style.visibility = "hidden";
   test.classList.toggle("block");
 });
+
 test.addEventListener("click", toggleMegaMenu);
